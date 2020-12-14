1e39ff336b0aff4b67d3e618e69a4a7d5ff59ad6
anchalsingh85305 <anchalsingh85305@gmail.com>
2020-12-14 22:05:18 +0530

    Anchal-blog

---
+++ my-app/src/pages/AboutPage.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const AboutPage = () => (
+    <React.Fragment>
+  <h1>About Us</h1>
+
+<p>
+    You can open another folder instead. – yushulx May 5 '15 at 3:14
+8
+You can now do File  Close Folder [Ctrl+K F], you should change the accepted answer skyline75489 – Markus Meskanen Jun 16 '17 at 7:49
+1
+For seriously puzzled inquirers, you must release the control key before pressing FThis isn't the way any other application works on windows, that I can tell. dudeNumber4 Oct 25 '19 at 18:02 
+</p>
+
+    </React.Fragment>
+  
+  );
+
+  export default AboutPage;
